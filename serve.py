4d447d8fbfaf79a805663e4ba82774c73dd52833
Alisamar Husain <35369637+zarath1810@users.noreply.github.com>
2018-09-21 14:42:17 +0530

    local

--- serve.py
+++ serve.py
@@ -1,4 +1,4 @@
 from CTFd import create_app
 
 app = create_app()
-app.run(debug=True, threaded=True, host="192.168.0.103", port=4000)
+app.run(debug=True, threaded=True, host="192.168.0.104", port=4000)

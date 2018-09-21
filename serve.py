33324dd4daaaff41ff94d8952d675c73b88d3b38
Daniyal Abbasi <abbasi.daniyal98@gmail.com>
2018-09-21 14:03:24 +0530

    Add question Joke (TAP)

--- serve.py
+++ serve.py
@@ -1,4 +1,4 @@
 from CTFd import create_app
 
 app = create_app()
-app.run(debug=True, threaded=True, host="192.168.0.103", port=4000)
+app.run(debug=True, threaded=True, host="192.168.29.211", port=4000)
